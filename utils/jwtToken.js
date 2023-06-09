9c40ac9d657f05360ecb0b67584687960abdf8d6
unknown <mrjanuaryx@gmail.com>
2023-06-09 14:52:21 +0530

    update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,16 +3,14 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
-
   //options for cookie
 
   const options = {
-    expires: new Date(
-      Date.now() + 6000000
-    ),
+    expires: new Date(Date.now() + 6000000),
     httpOnly: true,
+    sameSite: "None",
+    secure: true,
   };
-
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
