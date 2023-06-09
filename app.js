de804428d894a76fb12b2b79ce383e2960414272
unknown <mrjanuaryx@gmail.com>
2023-06-09 14:41:32 +0530

    update

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ const app = express();
 
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://majorbackend-production-0037.up.railway.app/",
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -48,7 +53,7 @@ cloudinary.v2.config({
       );
     });
   } catch (err) {
-    console.log(err,);
+    console.log(err);
   }
 })();
 
