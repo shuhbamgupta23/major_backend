4d95566761ab799320f51bec8df9b686c3f6ac74
Shubham <mrjanuaryx@gmail.com>
2023-09-11 00:36:17 +0530

    update

--- app.js
+++ app.js
@@ -16,12 +16,7 @@ const app = express();
 
 dotenv.config();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
