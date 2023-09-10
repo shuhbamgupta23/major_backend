e2f9e01b21b67c24038e79c2757e9d044a9ab769
Shubham <mrjanuaryx@gmail.com>
2023-09-11 00:41:53 +0530

    update

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const app = express();
 
 dotenv.config();
 app.use(express.json());
-app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -30,6 +29,7 @@ app.use("/", orderRouter);
 app.use("/", paymentRouter);
 //middleware for error
 app.use(errorMiddleware);
+app.use(cors());
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_NAME,
