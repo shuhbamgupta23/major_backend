f2d8d6c90d829783a8735ea16fae856f1e18adef
Shubham <mrjanuaryx@gmail.com>
2023-09-11 13:46:06 +0530

    update

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://major-frontend-ten.vercel.app"
+    "*"
   );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Methods", "*");
   next();
 });
 app.use(express.json());
